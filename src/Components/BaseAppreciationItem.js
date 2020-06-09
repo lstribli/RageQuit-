ea9e29da2201b7914843d15e163772639fa16129
Logan Stribling <logan.stribling@gmail.com>
2020-06-09 02:34:30 -0600

    added content to Discord and BaseApp

--- src/Components/BaseAppreciationItem.js
+++ src/Components/BaseAppreciationItem.js
@@ -4,7 +4,10 @@ import React from 'react';
 export default class BaseAppreciationItem extends React.Component {
   render() {
     return (
-      <div></div>
+      <div className="description">
+        <h1>Coming soon!</h1>
+        <h3>See some of the creative bases built by you, our all-important players!</h3>
+      </div>
     )
   }
 }
--- src/Components/Discord.js
+++ src/Components/Discord.js
@@ -1,10 +1,21 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom'
 
 export default class Discord extends React.Component {
   render() {
     return (
-      <div></div>
+      <div className="rules">
+        <h1>Join our Discord</h1>
+        <section className="description">
+          <h3>Open up your Discord App with this link:</h3>
+          <div className="talk">
+            <a
+              href="https://discord.gg/x9dKyZr" target="blank"
+            >
+            </a>
+          </div>
+        </section>
+      </div>
     )
   }
 }
